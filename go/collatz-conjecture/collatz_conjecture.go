2ed32149d708a767152fbd89e9e81c136c36945a
dhany <dhany@citra-digital.org>
2025-09-25 14:45:09 +0700

    CollatzConjecture

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,22 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return 0, errors.New("must be greater than 0")
+	}
+
+	count := 0
+
+	for n != 1 {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = (n * 3) + 1
+		}
+		count += 1
+	}
+
+	return count, nil
 }
